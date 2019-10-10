12bd333f269f31e3871c07a29b69528556165712
ryanjunglee1 <47785083+ryanjunglee1@users.noreply.github.com>
2019-10-10 17:50:17 -0400

    update

--- CraigslistNotifier/WebScraper/WebScraper.java
+++ CraigslistNotifier/WebScraper/WebScraper.java
@@ -11,19 +11,17 @@ public class WebScraper {
 	protected String subArea;
 	protected String topic;
 	protected ArrayList<String> subAreaList;
+	protected ArrayList<String> topicList;
 
 	public WebScraper(String area, String subArea, String topic) {
 		this.area = area;
 		this.subArea = subArea;
 		this.topic = topic;
+		subAreaList = new ArrayList<>();
+		topicList = new ArrayList<>();
 
 		try {
 			website = Jsoup.connect("https://" + area + ".craigslist.org/search/" + topic + "?").get();
-			Element mySubArea = website.getElementById("subArea");
-			Elements mySubAreas = mySubArea.getElementsByTag("option");
-			for (Element myElement: mySubAreas) {
-				System.out.println(myElement.attr("value"));
-			}
 		}
 		catch(Exception IOException) {
 			website = null;
@@ -47,4 +45,20 @@ public class WebScraper {
 	public String getsubArea() { return subArea; }
 
 	public String getTopic() { return topic; }
+
+	public void setSubAreaList() {
+		Element mySubArea = website.getElementById("subArea");
+		Elements mySubAreas = mySubArea.getElementsByTag("option");
+		for (Element myElement: mySubAreas) {
+			subAreaList.add(myElement.attr("value"));
+		}
+	}
+
+	public void setTopicList() {
+		Element mySubArea = website.getElementById("subCatAbb");
+		Elements mySubAreas = mySubArea.getElementsByTag("option");
+		for (Element myElement: mySubAreas) {
+			topicList.add(myElement.attr("value"));
+		}
+	}
 }
