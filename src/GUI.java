8ec096b405dc64d64424ba0d3068b755e3b4f8bc
lordartiman <artishala0@gmail.com>
2019-11-05 10:40:41 -0500

    Modifications to file organization

--- src/GUI.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-public class GUI {
-	public void start() {
-		JFrame mainWindow = new JFrame("Craigslist");
-		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		mainWindow.setSize(500,500);
-		mainWindow.setVisible(true);
-	}
-}
--- src/TestWebscraper.java
+++
@@ -1,8 +0,0 @@
-public class TestWebscraper {
-    public static void main(String args[]) {
-        WebScraper dc = new WebScraper("washingtondc");
-        System.out.println(dc.area);
-        GUI gui = new GUI();
-        gui.start();
-    }
-}
