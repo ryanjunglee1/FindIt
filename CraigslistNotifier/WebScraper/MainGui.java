6ab234108214662ef4b2a3c9528df52f165daf30
lordartiman <artishala0@gmail.com>
2019-11-17 13:25:16 -0500

    Updates to mainGUi

--- CraigslistNotifier/WebScraper/MainGui.java
+++ CraigslistNotifier/WebScraper/MainGui.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 
 import javax.swing.AbstractAction;
 import javax.swing.BorderFactory;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -47,31 +48,57 @@ public class MainGui implements ActionListener{
 		button.addActionListener(this);
 		search = new Search();
 		String[] statechoices = search.getStateMap().keySet().toArray(new String[0]);
+		String[] topicchoices = search.getTopicMap().keySet().toArray(new String[0]);
 		String[] testchoices = {"TEST", "TEST2", "TEST3"};
 		stateselect = new JComboBox<String>(statechoices);
 		stateselect.addItem("Choose a state");
 		stateselect.setSelectedIndex(stateselect.getItemCount() - 1);
-		stateselect.addActionListener(new AbstractAction("test") {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				System.out.println("Selected Item: " + (String)stateselect.getSelectedItem());
-				search = new Search();
-				search.setState((String)stateselect.getSelectedItem());
-				System.out.println("Search state: " + search.getState());
-				System.out.println("setAreaMap success: " + search.setAreaMap());
-				//String[] areachoices = search.getAreaMap().keySet().toArray(new String[0]);
-				//System.out.println(areachoices[0]);
-				//search.setAreaMap();
-				//System.out.println(search.hasArea());
-				//updateAreas();
-			}
-		});
-		updateAreas();
+		areaselect = new JComboBox<String>(testchoices);
 		subareaselect = new JComboBox<String>(testchoices);
 		topicselect = new JComboBox<String>(testchoices);
 		categoryselect = new JComboBox<String>(testchoices);
+		/*
+		areaselect.addActionListener(new AbstractAction("areaselect") {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (areaselect.getSelectedItem() == "N/A") {
+					//set a topic for the search without any specific area
+				} else {
+					System.out.println("Selected Item: " + (String)areaselect.getSelectedItem());
+					search.setArea((String)areaselect.getSelectedItem());
+					System.out.println("Search Area: " + search.getArea());
+					System.out.println("setSubAreaMap success: " + search.setSubAreaMap());
+					//areaselect.removeAllItems();
+					//updateAreas();
+				}
+			}
+		});
+		*/
+		stateselect.addActionListener(new AbstractAction("test") {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (stateselect.getSelectedItem() == "Choose a state") {
+					areaselect.removeAllItems();
+					areaselect.addItem("N/A");
+					subareaselect.removeAllItems();
+					subareaselect.addItem("N/A");
+					topicselect.removeAllItems();
+					topicselect.addItem("N/A");
+					categoryselect.removeAllItems();
+					categoryselect.addItem("N/A");
+					search = new Search();
+				} else {
+					System.out.println("Selected Item: " + (String)stateselect.getSelectedItem());
+					search = new Search();
+					search.setState((String)stateselect.getSelectedItem());
+					System.out.println("Search state: " + search.getState());
+					System.out.println("setAreaMap success: " + search.setAreaMap());
+					areaselect.removeAllItems();
+					updateAreas();
+				}
+			}
+		});
 		//updateSubAreas();
-		
 		
 		//adds the elements to the panel in grid layout
 		basicsearch.setBorder(BorderFactory.createEmptyBorder(30,30,10,10));
@@ -123,33 +150,49 @@ public class MainGui implements ActionListener{
 	}
 	
 	protected void updateAreas() {
-		if (stateselect.getSelectedIndex() == stateselect.getItemCount() - 1) {
-			areaselect = new JComboBox<String>(new String[]{"N/A"});
+		if (search.setAreaMap() == false) {
+			areaselect.addItem("N/A");
+			search = new Search();
+			search.setState((String) stateselect.getSelectedItem());
+			System.out.println("No areas, Search state: " + search.getState());
 		} else {
+			areaselect.removeAllItems();
 			//search.setState((String)stateselect.getSelectedItem());
-			System.out.println(search.getState());
-			System.out.println(search.setAreaMap());
+			System.out.println("State has areas, state: " + search.getState());
+			System.out.println("Has areas for state: " + search.setAreaMap() + " Areas: ");
 			String[] areachoices = search.getAreaMap().keySet().toArray(new String[0]);
-			areaselect = new JComboBox<String>(areachoices);
+			for (String s : areachoices) {
+				System.out.println(s);
+				if (!s.equals(""))
+					areaselect.addItem(s);
+			}
+			areaselect.setSelectedIndex(0);
+			search.setArea(areaselect.getSelectedItem().toString());
+			System.out.println("Search area set: " + areaselect.getSelectedItem().toString());
+		}
+		
+		if (search.setSubAreaMap()) {
+			System.out.println(search.getState() + " " + search.getArea() + " has subareas, updating selector");
+			updateSubAreas();
+		} else {
+			System.out.println("No subareas");
+			subareaselect.removeAllItems();
+			subareaselect.addItem("N/A");
 		}
 	}
 	
 	protected void updateSubAreas() {
-		String area = (String) areaselect.getSelectedItem();
-		System.out.println(area);
-		search.setArea(area);
-		System.out.println("Has area: " + search.hasArea);
-		if (search.setSubAreaMap() == false) {
-			subareaselect = new JComboBox<String>(new String[]{"N/A"});
-		} else {
-			System.out.println(search.setSubAreaMap());
-			String[] subareachoices = search.getSubAreaMap().keySet().toArray(new String[0]);
-			subareaselect = new JComboBox<String>(subareachoices);
+		String area = search.getArea();
+		System.out.println();
+		System.out.println("Has area: " + search.hasArea + " Search area: " + area);
+		String[] subareachoices = search.getSubAreaMap().keySet().toArray(new String[0]);
+		subareaselect.removeAllItems();
+		System.out.println("Choose from a list of sub areas for: " + search.getState() + " " + search.getArea());
+		for (String subArea: subareachoices) {
+			System.out.print(subArea + "     ");
+			subareaselect.addItem(subArea);
 		}
-		search.setTopicMap();
-		String[] topics = search.getTopicMap().keySet().toArray(new String[0]);
-		topicselect = new JComboBox<String>(topics);
-		updateCategories();
+		//search.setSubArea(scan.nextLine());
 		
 	}
 	
