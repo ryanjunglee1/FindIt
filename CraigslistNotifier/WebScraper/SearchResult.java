8b0b34d967c3b0a6f435811e2ccff39e0c139f41
lordartiman <artishala0@gmail.com>
2019-11-26 11:30:46 -0500

    created searchresult gui and fixed keyword search
    Keyword search was putting two question marks for query in the URL, this issue has been fixed.
    SearchResult GUI with table to visually display data created when a search is made

--- CraigslistNotifier/WebScraper/SearchResult.java
+++ CraigslistNotifier/WebScraper/SearchResult.java
@@ -1,6 +1,13 @@
+import java.awt.BorderLayout;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Iterator;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 /*
  * This class is used to contain an arrayList of items , the SearchQuery used to get those items, and update methods
@@ -10,6 +17,7 @@ import java.util.Iterator;
  * @version 1.0
  */
 public class SearchResult {
+	//data fields
 	protected ArrayList<Item> itemList;
 	protected int resultSize;
 	protected SearchQuery query;
@@ -17,9 +25,30 @@ public class SearchResult {
 	protected boolean willUpdate;
 	protected String updateEmail;
 	
-	public SearchResult(SearchQuery q) {
-		itemList = new ArrayList<Item>();
-		this.query = q;
+	//GUI elements
+	private JFrame frame = new JFrame();
+	private JPanel panel = new JPanel();
+	private JLabel label = new JLabel("Test");
+	private JTable table;
+	private JScrollPane scrollpane;
+	private String[] columnNames = {"Item Name", "Item Price"};
+	
+	//initialize the searchresult gui with the list of items provided by searchquery
+	public SearchResult(ArrayList<Item> results, String title) {
+		this.itemList = results;
+		this.lastUpdated = LocalTime.now();
+		table = new JTable(makeData(), columnNames);
+		scrollpane = new JScrollPane(table);
+		table.setFillsViewportHeight(true);
+		panel.add(label);
+		panel.add(scrollpane);
+		frame.add(panel);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setTitle(title);
+        frame.setSize(500, 500);
+        //frame.pack();
+        frame.setVisible(true);
+		//this.query = q;
 	}
 	
 	public void add(Item i) {
@@ -32,5 +61,15 @@ public class SearchResult {
 			Item i = (Item) itemIterator.next();
 			System.out.println(i);
 		}
+	}
+	
+	public Object[][] makeData() {
+		Object[][] data = new Object[this.itemList.size()][2];
+		for (int i = 0; i < this.itemList.size(); i++) {
+			Item it = this.itemList.get(i);
+			data[i][0] = it.itemName;
+			data[i][1] = it.itemPrice;
+		}
+		return data;
 	}
 }
