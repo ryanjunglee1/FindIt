68ca9455e9491ac572fa54b995fc54bf1c5cbf44
lordartiman <artishala0@gmail.com>
2019-12-03 18:27:56 -0500

    SearchResult GUI Updates

--- CraigslistNotifier/WebScraper/SearchResult.java
+++ CraigslistNotifier/WebScraper/SearchResult.java
@@ -1,6 +1,7 @@
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Desktop;
+import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.net.URI;
@@ -8,12 +9,17 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import javax.swing.AbstractAction;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.TableCellRenderer;
 
@@ -35,31 +41,71 @@ public class SearchResult {
 	
 	//GUI elements
 	private JFrame frame = new JFrame();
+	private JPanel notificationPanel = new JPanel();
 	private JPanel panel = new JPanel();
-	private JLabel label = new JLabel("Test");
+	private JPanel container = new JPanel();
+	private JLabel label = new JLabel("Enter your email for notifications");
+	private JTextField email = new JTextField(20);
+	private JTextField updateIntervalField = new JTextField(5);
+	private JLabel updateLabel1 = new JLabel("Check for updates every: ");
+	private JLabel updateLabel2 = new JLabel("Minutes");
+	private JButton updateButton = new JButton("Enable");
 	private JTable table;
 	private JScrollPane scrollpane;
 	private String[] columnNames = {"Item Name", "Item Price", "button"};
 	
 	//initialize the searchresult gui with the list of items provided by searchquery
 	public SearchResult(ArrayList<Item> results, String title) {
+		
+		//itemlist set by constructor args, JTable populated with makedata method of searchresult and tablemodel set with mouselistener
 		this.itemList = results;
 		this.resultSize = this.itemList.size();
 		this.lastUpdated = LocalTime.now();
 		table = new JTable(makeData(), columnNames);
 		table.setModel(new ItemTableModel(this));
 		table.addMouseListener(new JTableButtonMouseListener(this.table));
-		//table.setDefaultEditor(Object.class, null);
+		
+		//scrollpanel created and table added to scroll panel with button cell renderer
 		scrollpane = new JScrollPane(table);
 		table.setFillsViewportHeight(true);
 		TableCellRenderer buttonRenderer = new JTableButtonRenderer();
 		table.getColumn("button").setCellRenderer(buttonRenderer);
-		panel.add(label);
+		
+		//notification panel elements populated
+		notificationPanel.add(label);
+		notificationPanel.add(email);
+		notificationPanel.add(updateLabel1);
+		notificationPanel.add(updateIntervalField);
+		notificationPanel.add(updateLabel2);
+		notificationPanel.add(updateButton);
+		updateButton.addActionListener(new AbstractAction("Enable") {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (willUpdate) {
+					willUpdate = false;
+					System.out.println("disabled");
+				} else {
+					willUpdate = true;
+					System.out.println("enabled");
+				}
+				
+			}
+			
+		});
+		notificationPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+		
+		//panel element (table) populated
 		panel.add(scrollpane);
-		frame.add(panel);
+		
+		//container layout set and panel + notificatonpanel added, container added to frame and frame initialized
+		container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
+		container.add(panel);
+		container.add(notificationPanel);
+		frame.add(container);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setTitle(title);
-        frame.setSize(500, 500);
+        frame.setSize(500, 650);
         //frame.pack();
         frame.setVisible(true);
 		
