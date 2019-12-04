852d7236246edbb7d89ef4f2ec69dbfcb71f846b
ryanjunglee1 <47785083+ryanjunglee1@users.noreply.github.com>
2019-12-03 20:50:40 -0500

    MailSender works now

--- CraigslistNotifier/WebScraper/MailSender.java
+++ CraigslistNotifier/WebScraper/MailSender.java
@@ -2,11 +2,23 @@ import java.util.*;
 import javax.mail.*;
 import javax.mail.internet.*;
 import javax.activation.*;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+/**
+ * 
+ * @author Ryan Lee
+ *
+ */
 public class MailSender {
 	private static final String sender = "craigslistnotifier1@gmail.com";
-	private static final String password = "CS211project";
+	private static final String password = Password.password;
 	private String recipient, subject, body;
+	/**
+	 * Initializes a MailSender object which can notify users via email
+	 * @param recipient
+	 * @param subject
+	 * @param body
+	 */
 	
 	public MailSender(String recipient, String subject, String body) {
 		this.recipient = recipient;
@@ -14,6 +26,10 @@ public class MailSender {
 		this.body = body;
 	}
 	
+	/**
+	 * Sends the email to the user
+	 * @return true if mail is sent, false if not
+	 */
 	public boolean sendMail() {
 		Properties prop = new Properties();
 		prop.put("mail.smtp.host", "smtp.gmail.com");
@@ -36,7 +52,6 @@ public class MailSender {
 			return true;
 		}
 		catch (MessagingException e) {
-			e.printStackTrace();
 			return false;
 		}
 	}
